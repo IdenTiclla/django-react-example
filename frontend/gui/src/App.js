3a29c093c67055dde28cf39f898463bc6bef71b3
unknown <iden.ticlla@gmail.com>
2021-04-05 11:58:37 -0400

    ArticleDetailView working

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 
+import { BrowserRouter as Router } from 'react-router-dom'
 
+import BaseRouter from './routes'
 
 import CustomLayout from './containers/Layout'
 import ArticleList from './containers/ArticleListView'
+
 
 
 class App extends Component{
     render() {
         return (
            <div className="App">
-               <CustomLayout>
-                    <ArticleList></ArticleList>
-               </CustomLayout>
+               <Router>
+                    <CustomLayout>
+                        <BaseRouter></BaseRouter>
+                    </CustomLayout>
+               </Router>
+               
            </div> 
         )
     }
